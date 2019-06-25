8fa3afb18215f475de1a075949c016fda0a7194c
Rahul Shah <rahulsanjay18@gmail.com>
2019-06-24 21:18:43 -0400

    programmed bernoulli numbers

--- NoteGenerator.java
+++ NoteGenerator.java
@@ -1,5 +1,61 @@
 public class NoteGenerator {
 
+    static double getSummandOfPower(int exponent){
+      double bernoulliNum = getBernoulliNumber(exponent);
+
+
+
+      return 0;
+    }
+
+    static double getBernoulliNumber(int n){
+      double bernoulliNum = 0;
+      for(int k = 0; k <= n; k++){
+        double divisor = (double) 1 /(k + 1);
+
+        double sum = 0;
+        for(int v = 0; v <= k; v++){
+          int negMultiplier = (v % 2 == 0) ? 1 : -1;
+
+          int vPlus1 = v + 1;
+          int raisedToPower = exp(vPlus1, n);
+
+          double combination = getCombination(k, v);
+
+          sum += negMultiplier * vPlus1 * combination;
+        }
+
+        bernoulliNum += sum * divisor;
+      }
+
+      return bernoulliNum;
+    }
+
+    static double getCombination(int top, int bottom){
+      double numerator = factorial(top);
+      double denominator = factorial(top - bottom) * factorial(bottom);
+
+      return numerator / denominator;
+    }
+
+    static int factorial(int n){
+      int total = 1;
+
+      for(int i = 1; i <= n; i++){
+        total *= i;
+      }
+
+      return total;
+    }
+
+    static int exp(int base, int exponent){
+      int total = 1;
+
+      for(int i = 0; i < exponent; i++){
+        total *= base;
+      }
+      return total;
+    }
 
     static double getNoteOfFunction(char c, double constant, int i){
         switch (c){
